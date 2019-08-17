3457fbcfe8be84ac722753e8b79da81c240c79ff
volk <matiasdylanc@gmail.com>
2019-08-17 02:21:25 -0300

    Template added

---
+++ src/public/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    const socket = io();
+
+    // obtaining DOM elements from the interface
+    const $messageForm = $('#message-form');
+    const $messageInput = $('#message');
+    const $chat = $('#chat');
+})
+
+// events
+
+$messageForm.submit( e => {
+    e.preventDefault();
+    console.log('enviando')
+    })
+
